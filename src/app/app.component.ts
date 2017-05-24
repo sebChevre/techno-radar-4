520c774602e89bf9d952783223dbe6ad4e8a37a0
seb <seb.chevre>
2017-05-24 07:34:35 +0200

    Starter ok

---
+++ src/app/app.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+import '../assets/css/styles.css';
+
+@Component({
+    selector: 'techno2-app',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
+})
+export class AppComponent { }
