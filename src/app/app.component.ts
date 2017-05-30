d4e388f9492aa4fb47cfef51845fce628d7a0450
seb <seb.chevre>
2017-05-30 16:29:56 +0200

    feature d3 ok

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,13 @@
 import { Component } from '@angular/core';
 import '../assets/css/styles.css';
 
+import * as d3 from 'd3-selection';
+import * as d3Scale from 'd3-scale';
+import * as d3Shape from "d3-shape";
+import * as d3Array from "d3-array";
+import * as d3Axis from "d3-axis";
+import {Router, ActivatedRoute} from "@angular/router";
+
 @Component({
     selector: 'my-app',
     templateUrl: './app.component.html',
@@ -9,4 +16,35 @@ import '../assets/css/styles.css';
         require('./app.style.less').toString()
     ]
 })
-export class AppComponent { }
+export class AppComponent {
+
+    private margin = {top: 20, right: 20, bottom: 30, left: 50};
+    private width: number;
+    private height: number;
+    private x: any;
+    private y: any;
+    private svg: any;
+
+
+    ngOnInit(): void {
+        d3.select("body")
+            .append("span")
+        .text("hello world");
+    }
+
+    constructor(
+       // private route: ActivatedRoute,
+        //private router: Router
+    ) {
+
+
+        /*
+            .append("g")
+            .attr("transform", "translate(" + this.margin.left + "," + this.margin.top + ")");
+*/
+
+
+        // console.log(radar);
+
+    }
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule }  from '@angular/platform-browser';
 import { AppComponent } from './app.component';
+import {RouterModule} from "@angular/router";
 @NgModule({
     imports: [
-        BrowserModule
+        BrowserModule,
     ],
     declarations: [
         AppComponent
